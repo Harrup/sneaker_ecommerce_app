f6eacdc6c32b6bb659001e022175b15e8209f88c
Satpal Singh <harrup.bamrah@gmail.com>
2019-03-18 20:39:15 -0400

    add for each on all.edge

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -2,7 +2,8 @@
 
 class ProductController {
   index({view}){
-    return view.render('Products/all')
+    const products = [1,2,3,4,5,6,7,8,9,10]
+    return view.render('Products/all', {products})
   }
 }
 
