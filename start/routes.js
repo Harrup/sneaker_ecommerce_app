2300ec0c9022160ae40808d611cd6fb61434d97d
Satpal Singh <harrup.bamrah@gmail.com>
2019-03-21 20:37:43 -0400

    add html show page

--- start/routes.js
+++ start/routes.js
@@ -20,6 +20,7 @@ Route.get('/', 'PageController.home')
 Route.get('/about', 'PageController.about')
 Route.get('/products', 'ProductController.index')
 Route.get('/products/new-arrivals', 'ProductController.newArrivals')
+Route.get('/:brand/:subcategory/:name', 'ProductController.show')
 
 //User Authentication
 Route.get('/account/login', 'AuthController.login')
